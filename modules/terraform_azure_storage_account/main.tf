92e9a7316afd633c51b1840cb88934941e421620
rinkeshputtademo <rinkeshputta@outlook.com>
2024-11-15 17:06:15 +0530

    terraform initial code for stage1

--- modules/terraform_azure_storage_account/main.tf
+++ modules/terraform_azure_storage_account/main.tf
@@ -1,16 +1,16 @@
 // Deploy the storage account
 //**********************************************************************************************
 resource "azurerm_storage_account" "storage_account_01" {
-  name                      = "${var.env}-${var.postfix}-${var.storage_account_name}"
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  account_tier              = var.storage_account_tier
-  account_replication_type  = var.storage_account_replication_type
-  account_kind              = var.storage_account_kind
-  access_tier               = var.storage_account_access_tier
-  allow_blob_public_access  = var.storage_account_allow_blob_public_access
-  enable_https_traffic_only = var.storage_account_enable_https_traffic_only
-  min_tls_version           = var.storage_account_tls_version
-  tags                      = var.tags
+  name                          = "${var.env}-${var.postfix}-${var.storage_account_name}"
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  account_tier                  = var.storage_account_tier
+  account_replication_type      = var.storage_account_replication_type
+  account_kind                  = var.storage_account_kind
+  access_tier                   = var.storage_account_access_tier
+  public_network_access_enabled = var.storage_account_allow_blob_public_access
+  https_traffic_only_enabled    = var.storage_account_enable_https_traffic_only
+  min_tls_version               = var.storage_account_tls_version
+  tags                          = var.tags
 }
 //**********************************************************************************************
